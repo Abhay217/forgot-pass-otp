647f8a45a16cbfda59edc04164690e297fc0cb50
Abhay Shukla <79450769+Abhay217@users.noreply.github.com>
2021-06-10 15:34:44 +0530

    Add files via upload

---
+++ forgot/assets/js/verification.js
@@ -0,0 +1,60 @@
+function sendOTP() {
+	$(".error").html("").hide();
+	var number = $("#mobile").val();
+	
+	if (number.length == 10 && number != null) {
+		// var input = {
+		// 	"mobile_number" : number,
+		// 	"action" : "send_otp"
+		// };
+		// console.log(number.length);
+		$.ajax({
+			url : 'controller.php',
+			type : 'POST',
+			data :{
+				"mobile_number" : number,
+				"action" : "send_otp"
+			},
+			success : function(response) {
+				$(".container").html(response);
+			}
+		});
+	} else {
+		$(".error").html('Please enter a valid number!')
+		$(".error").show();
+	}
+}
+
+function verifyOTP() {
+	$(".error").html("").hide();
+	$(".success").html("").hide();
+	var otp = $("#mobileOtp").val();
+	// var input = {
+	// 	"otp" : otp,
+	// 	"action" : "verify_otp"
+	// };
+	if (otp.length == 6 && otp != null) {
+	console.log(otp);
+
+		$.ajax({
+			url : 'controller.php',
+			type : 'POST',
+			dataType : "json",
+			data : {
+				"otp" : otp,
+				"action" : "verify_otp"
+			},
+			success : function(response) {
+				$("." + response.type).html(response.message)
+				$("." + response.type).show();
+				window.location = 'http://localhost/forgot/login-forget/reset-pass.php';
+			},
+			error : function() {
+				alert("ss");
+			}
+		});
+	} else {
+		$(".error").html('You have entered wrong OTP.')
+		$(".error").show();
+	}
+}
---
+++ forgot/login-forget/done.php
@@ -0,0 +1,6 @@
+<?php
+echo "youre password is reset";
+?>
+<form action="http://localhost/forgot/login-forget/index.php" method="POST">
+<button name="went to login page" type="went to login page" class="btn btn-danger px-4 py-2 text-uppercase white font-small-4 box-shadow-2 border-0" value="went to login page">login page </button>
+</form>
